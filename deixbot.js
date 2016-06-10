ed1a03262b733c7f7334df0f0b655ae9d70bb5ca
Daniel Dent <Deixel@users.noreply.github.com>
2016-06-10 21:45:33 +0100

    Refactor and tidy up scoping

--- deixbot.js
+++ deixbot.js
@@ -1,73 +1,64 @@
 var Discord = require("discord.js");
 var config = require("./config");
 
-var mybot = new Discord.Client();
-var myUser;
-mybot.on("message", function(message) {
-	if(message.content == "ping")
-	{
-		mybot.reply(message, "pong");
+var client = new Discord.Client();
+var botUser;
+client.on("message", function(message) {
+	if(message.content == "ping") {
+		client.reply(message, "pong");
 	}
-	if(message.content == "!help")
-	{
+	if(message.content == "!help") {
 		var msg = "You need help " + message.author;
-		mybot.sendMessage(message.channel, msg);
+		client.sendMessage(message.channel, msg);
 	}
-	if(message.content.toLowerCase().indexOf("hello") > -1 && message.isMentioned(myUser))
-	{
+	if(message.content.toLowerCase().indexOf("hello") > -1 && message.isMentioned(botUser)) {
 		var msg =  "Hello " + message.author;
-		mybot.sendMessage(message.channel, msg);
+		client.sendMessage(message.channel, msg);
 	}
-	if(message.content == "!join")
-	{
-		mybot.joinVoiceChannel(message.author.voiceChannel, function(error, voiceConnection)
+	if(message.content == "!join") {
+		client.joinVoiceChannel(message.author.voiceChannel, function(error, voiceConnection)
 		{
-			if(error)
-			{
-				console.error(error);
+			if(error) {
+				return console.error(error);
 			}
-			mybot.voiceConnection.playFile("sound.mp3", function(error, intent)
-			{
-				if(error)
-				{
-					console.error(error);
+			voiceConnection.playFile("sound.mp3", function(error, intent) {
+				if(error) {
+					return console.error(error);
 				}
-				intent.once("end", playbackFinished);
+				intent.on("error", function(error) {
+					return console.error(error);
+				});
+				intent.once("end", function() {
+					client.leaveVoiceChannel(voiceConnection);
+				});
 			});
 		});
 	}
 });
 
-function playbackFinished()
-{
-	setTimeout(function(){mybot.leaveVoiceChannel(myUser.voiceChannel)}, 5000);
-}
-
-mybot.on("ready", function()
-{
-	mybot.setPlayingGame("with your emotions");
-	myUser = mybot.users.get("username", "DeixBot");
-	for (var i = 0; i < mybot.servers.length; i++)
+client.on("ready", function() {
+	client.setPlayingGame("with your emotions");
+	botUser = client.users.get("username", "DeixBot");
+	for (var i = 0; i < client.servers.length; i++)
 	{
-		var server = mybot.servers[i];
+		var server = client.servers[i];
 		var msg = "S'up " + server.name + "!";
-		mybot.sendMessage(server.defaultChannel, msg);
+		client.sendMessage(server.defaultChannel, msg);
 	}
 });
 
-process.on("SIGINT", function()
-{
-	for (var i = 0; i < mybot.servers.length; i++)
+process.on("SIGINT", function() {
+	for (var i = 0; i < client.servers.length; i++)
 	{
-		var server = mybot.servers[i];
-		mybot.sendMessage(server.defaultChannel, "Peace out!", function(i){
-			if(i + 1 == mybot.servers.length)
+		var server = client.servers[i];
+		client.sendMessage(server.defaultChannel, "Peace out!", function(i){
+			if(i + 1 == client.servers.length)
 			{
-				mybot.logout();
+				client.logout();
 			}
 		});
 	}
 });
 
 
-mybot.loginWithToken(config.discord.key);
+client.loginWithToken(config.discord.key);
