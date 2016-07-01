1ae913dbe5ef82439b333c2e52688bc282ac4bb1
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-01 20:44:13 +0100

    Add sample config file

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.cmdprefix = '!';
+config.playing = ["with your emotions", "with fire", "with myself"];
+config.bennyHill = "./bennyHill.mp3";
+config.bhvol = 0.25
+config.discord = {};
+
+config.discord.key = 'Discord API Key';
+config.discord.id = 'Discord User ID';
+
+module.exports = config;
