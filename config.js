fd6f845f495d7f3b736369f729ef97aa485a5224
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-02 20:59:53 +0100

    Updated default config file

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 var config = {};
 
-config.cmdprefix = '!';
-config.playing = ["with your emotions", "with fire", "with myself"];
-config.bennyHill = "./bennyHill.mp3";
-config.bhvol = 0.25
-config.discord = {};
+config.apikey = "Discord API Key";
 
-config.discord.key = 'Discord API Key';
-config.discord.id = 'Discord User ID';
+config.mysql = {};
+config.mysql.host = "localhost";
+config.mysql.user = "user";
+config.mysql.pass= "password";
+config.mysql.db = "database";
 
 module.exports = config;
