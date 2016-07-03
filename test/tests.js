ae64a3fa1f07e6de1d78aaccad77402e7bb73629
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-03 13:05:19 +0100

    Removed a rogue S

--- test/tests.js
+++ test/tests.js
@@ -10,7 +10,7 @@ describe("Command", function() {
 		it("should return a command if command exists", function() {
 			var pingCmd =  cmds.get("ping");
 			assert.property(pingCmd, "description");
-			assert.equals(pingCmd.description, "It's like ping-pong, but with words.");
+			assert.equal(pingCmd.description, "It's like ping-pong, but with words.");
 		});
 	});
 });
