3598d58826859a8c9aea7a713d5dbcd4e27ad551
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-03 13:16:32 +0100

    Added some more unit tests

--- test/tests.js
+++ test/tests.js
@@ -2,20 +2,31 @@ var assert =  require("chai").assert;
 var cmds = require("../commands/commands");
 
 
-describe("Command", function() {
-	describe("#get()", function() {
+describe("Commands", function() {
+	describe("get()", function() {
 		it("should return null if command doesn't exist", function() {
 			assert.equal(null, cmds.get("test"));
 		});
-		var pingCmd =  cmds.get("ping");
+
 		it("should return a command if command exists", function() {
+			var pingCmd =  cmds.get("ping");
+			assert.property(pingCmd, "cmd");
 			assert.property(pingCmd, "description");
+			assert.property(pingCmd, "action");
+			assert.property(pingCmd, "hidden");
 		});
 		it("should return the correct command.", function() {
-			assert.notEqual(pingCmd.description, "Assigns blame appropriately");
-			assert.equal(pingCmd.description, "It's like ping-pong, but with words.");
+			var blameCmd = cmds.get("blame");
+			assert.equal(blameCmd.description, "Assigns blame appropriately");
+			assert.notEqual(blameCmd.description, "It's like ping-pong, but with words.");
 		});
-
-
+		it("hidden commands should have a hidden property", function() {
+			var configCmd = cmds.get("config");
+			assert.equal(configCmd.hidden, true);
+		});
+		it("visible commands should not have a hidden property", function() {
+			var geCmd = cmds.get("ge");
+			assert.equal(geCmd.hidden, false);
+		});
 	});
 });
