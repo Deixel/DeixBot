39e73624f535fca625fe763ceb8348c2efccb344
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-03 12:48:00 +0100

    Added second unit test

--- test/tests.js
+++ test/tests.js
@@ -3,8 +3,11 @@ var cmds = require("../commands/commands");
 
 describe("Command", function() {
 	describe("#get()", function() {
-		it("should return null if command doesn't exist", function () {
+		it("should return null if command doesn't exist", function() {
 			assert.equal(null, cmds.get("test"));
+		});
+		it("should not return a command if command exists", function() {
+			assert.typeOf(cmds.get("ping"), Command);
 		});
 	});
 });
