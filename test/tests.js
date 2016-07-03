92323671f931baa249315df79ec3147b59c0e2c9
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-03 12:49:49 +0100

    Updated unit tests

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,20 @@
 var assert =  require("chai").assert;
 var cmds = require("../commands/commands");
 
+function Command(cmd, descr, action, hidden = false) {
+	this.cmd = cmd;
+	this.description = descr;
+	this.action = action;
+	this.hidden = hidden;
+	//commands[cmd]=this;
+}
+
 describe("Command", function() {
 	describe("#get()", function() {
 		it("should return null if command doesn't exist", function() {
 			assert.equal(null, cmds.get("test"));
 		});
-		it("should not return a command if command exists", function() {
+		it("should return a command if command exists", function() {
 			assert.typeOf(cmds.get("ping"), Command);
 		});
 	});
