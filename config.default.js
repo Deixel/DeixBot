20473222146941aa914519ee101cf8b331b4a30f
Daniel Dent <Deixel@users.noreply.github.com>
2016-07-19 01:37:09 +0100

    Added some debug

--- config.default.js
+++ config.default.js
@@ -17,9 +17,11 @@ exports.serverConfig = serverConfig;
 
 exports.getServerConfig = function getServerConfig(server, property) {
 	if(typeof serverConfig[server.id][property] !== undefined) {
+		console.log(serverConfig[server.id][property]);
 		return serverConfig[server.id][property];
 	}
 	else {
+		console.log(server.conifg["default"][property]);
 		return serverConfig["default"][property];
 	}
 };
