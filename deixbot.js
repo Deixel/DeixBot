1178f2820182a4cb85a7a6f12b84403a4b725312
Daniel Dent <Deixel@users.noreply.github.com>
2016-08-01 15:07:14 +0100

    Fixed unloading commands

--- deixbot.js
+++ deixbot.js
@@ -57,7 +57,7 @@ commands.unload = {
 		if(message.author.id == appConfig.ownerid) {
 			try {
 				delete commands[params[0]];
-				delete require.cache["./commands/" + params[0] + ".js"];
+				delete require.cache[require.resolve("./commands/" + params[0] + ".js")];
 				client.sendMessage(message.channel, "Successfully unloaded " + params[0]);
 				log.info("Unloaded " + params[0]);
 			}
