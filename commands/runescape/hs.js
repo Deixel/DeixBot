17406798200c7e558ea0e07219b42329807ae437
Daniel Dent <Deixel@users.noreply.github.com>
2017-04-22 13:38:47 +0100

    Updated hs to use typing rather than loading message

--- commands/runescape/hs.js
+++ commands/runescape/hs.js
@@ -19,24 +19,24 @@ module.exports = class HsCommand extends commando.Command {
 	}
 
 	async run(msg, args) {
-		msg.say('Loading...').then( (reply) => {
-			const player = args.player.replace(/ /, '_');
-			const http = require('http');
-			http.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + player, (res) => {
-				res.setEncoding('utf8');
-				var hsRaw = '';
-				res.on('data', (d) => hsRaw = hsRaw.concat(d));
-				res.on('end', () => {
-					let numSkills = 27;
-					let skillRaw = hsRaw.split('\n');
-					let skillNames = require('../../resources/rs-skill-names');
-					let	output = '';
-					for(let i = 0; i <= numSkills; i++) {
-						var sTemp = skillRaw[i].split(',');
-						output = output.concat(skillNames[i] + ': ' + sTemp[2] + ' (' + sTemp[1] + ')\n');
-					}
-					reply.edit('**' + player + '\'s Skills **\n```Javascript\n' + output + '```');
-				});
+		msg.channel.startTyping();
+		const player = args.player.replace(/ /, '_');
+		const http = require('http');
+		http.get('http://services.runescape.com/m=hiscore/index_lite.ws?player=' + player, (res) => {
+			res.setEncoding('utf8');
+			var hsRaw = '';
+			res.on('data', (d) => hsRaw = hsRaw.concat(d));
+			res.on('end', () => {
+				let numSkills = 27;
+				let skillRaw = hsRaw.split('\n');
+				let skillNames = require('../../resources/rs-skill-names');
+				let	output = '';
+				for(let i = 0; i <= numSkills; i++) {
+					var sTemp = skillRaw[i].split(',');
+					output = output.concat(skillNames[i] + ': ' + sTemp[2] + ' (' + sTemp[1] + ')\n');
+				}
+				msg.channel.stopTyping();
+				msg.channel.sendMessage('**' + player + '\'s Skills **\n```Javascript\n' + output + '```');
 			});
 		});
 	}
