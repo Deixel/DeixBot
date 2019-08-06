f2ce9ad52f863160f40ac350611cc980774d1814
Deixel <daniel@your-imagination.com>
2019-08-07 00:15:03 +0100

    Updated logger

--- logger.js
+++ logger.js
@@ -1,30 +1,25 @@
 var winston = require('winston');
 var dateFormat = require('dateformat');
-module.exports = new winston.Logger ({
+const {combine, timestamp, printf, } = winston.format;
+
+const myFormat = printf( ({level, message, timestamp }) => {
+	return `${timestamp} [${level}]: ${message}`;
+});
+module.exports = winston.createLogger ({
+	format: combine(
+		timestamp(),
+		myFormat
+	),
 	transports: [
 		new winston.transports.Console({
-			timestamp: function() {
-				var now = new Date();
-				return dateFormat(now, 'yyyy-mm-dd HH:MM:ss');
-			},
 			level: 'debug',
-			json: false,
-			colorize: true,
-			prettyPrint: true
 		}),
 		new winston.transports.File({
-			timestamp: function() {
-				var now = new Date();
-				return dateFormat(now, 'yyyy-mm-dd HH:MM:ss');
-			},
 			level: 'info',
 			filename: './logs/deixbot.log',
-			maxSize: 100000,
+			maxsize: 100000,
 			maxFiles: 10,
-			json: false,
-			prettyPrint: true,
 			tailable: true,
-			handleExceptions: true
 		})
 	]
 });
