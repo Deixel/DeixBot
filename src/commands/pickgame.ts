439d4ce750a3de23658241a4e7ffd7fcc8ef44c3
Deixel <Deixel@users.noreply.github.com>
2021-05-25 23:46:39 +0100

    Finished pickgame

--- src/commands/pickgame.ts
+++ src/commands/pickgame.ts
@@ -126,9 +126,10 @@ class PickGame extends DeixBotCommand {
 			let newGame = interaction.options[0].options?.[0].value;
 
 			let db = await open({filename: "./deixbot.sqlite", driver: sqlite3.Database});
-			db.run("INSERT INTO pickgame ( gameName ) VALUES ( ? )", newGame).then( () => {
-				interaction.followUp("Added `" + newGame +"` to the list!");
-			});
+			await db.run("INSERT INTO pickgame ( gameName ) VALUES ( ? )", newGame);
+			interaction.followUp("Added `" + newGame +"` to the list!");
+			db.close();
+			
 		}
 		catch (err) {
 			log.error(err);
@@ -138,91 +139,22 @@ class PickGame extends DeixBotCommand {
 
 	async remove(interaction:Discord.CommandInteraction)
 	{
-		interaction.reply("Game removed");
+		try {
+			await interaction.defer();
+			let oldGame = interaction.options[0].options?.[0].value;
+
+			let db = await open({filename: "./deixbot.sqlite", driver: sqlite3.Database});
+			await db.run("DELETE FROM pickgame WHERE gameId = (SELECT gameId FROM pickgame WHERE gameName = ? LIMIT 1)", oldGame)
+			interaction.followUp("Removed `" + oldGame +"` from the list!");
+			db.close();
+		}
+		catch (err) {
+			log.error(err);
+			interaction.followUp("Sorry, something went wrong!", {ephemeral: true});
+		}
+
+		
 	}
 }
 
 export default new PickGame();
-/*
-module.exports = class BlameCommand extends commando.Command {
-	constructor(client) {
-		super(client, {
-			name: "pickgame",
-			group: "misc",
-			memberName: "pickgame",
-			description: "Can't decide on something to play? Let me pick for you.",
-			args: [
-				{
-					key: "operation",
-					type: "string",
-					prompt: "what do you want to do?",
-					default: "pick",
-					validate: (value) => {
-						return value === "add" || value === "remove" || value === "pick" || value === "list" || value === "please";
-					}
-				},
-				{
-					key: "game",
-					type: "string",
-					prompt: "what game to do you want to add/remove?",
-					default: ""
-				}
-			]
-		});
-	}
-
-	async run(msg, args) {
-		sqlite.open("./deixbot.sqlite").then( (db) => {
-			if(args.operation === "pick" || args.operation === "please") {
-				msg.channel.startTyping();
-				db.get("SELECT gameName FROM pickgame ORDER BY RANDOM() LIMIT 1").then( (game) => {
-					msg.channel.stopTyping();
-					if(game) {
-						if(args.operation === "please") {
-							return msg.channel.send(`And the winner is: \`${game.gameName}\`!`);
-						}
-						else {
-							return msg.channel.send("Why are you asking me? You're just going to play Overwatch anyway...");
-						}
-					}
-					else {
-						return msg.channel.send("Couldn't find any games in the list");
-					}
-				}).catch(log.error);
-			}
-			else if(args.operation === "add") {
-				msg.channel.startTyping();
-				db.run("INSERT INTO pickgame ( gameName ) VALUES ( ? )", args.game).then( () => {
-					msg.channel.stopTyping();
-					return msg.channel.send(`Added ${args.game} to the list`);
-				}).catch( (err) => {
-					msg.stopTyping();
-					msg.channel.send("Whoops. Something went wrong. Sorry :frowning:");
-					return log.error(err);
-				});
-			}
-			else if(args.operation === "remove") {
-				msg.channel.startTyping();
-				db.run("DELETE FROM pickgame WHERE gameId = (SELECT gameId FROM pickgame WHERE gameName = ? LIMIT 1)", args.game).then( () => {
-					msg.channel.stopTyping();
-					return msg.channel.send(`Removed ${args.game} from the list`);
-				}).catch(log.error);
-			}
-			else if(args.operation === "list") {
-				msg.channel.startTyping();
-				db.all("SELECT gameName FROM pickgame").then( (rows) => {
-					if(rows.length === 0) {
-						msg.channel.stopTyping();
-						return msg.channel.send("There are no games in the list right now!");
-					}
-					else {
-						var gameList = "`" + rows.map( g => g.gameName ).join("` `") + "`";
-						msg.channel.stopTyping();
-						return msg.channel.send(`**Games to Choose From**\n${gameList}`);
-					}
-				}).catch(log.error);
-			}
-		});
-	}
-};
-*/
